d75cf246378191b0466b4d299777fac8e89d9073
PoppyHanna <142433975+PoppyHanna@users.noreply.github.com>
2025-05-19 20:43:50 -0400

    up 3

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,25 +1,24 @@
-import HttpError from 'http-errors'; 
-
+import { HttpError } from 'http-errors';
 
 export const errorHandler = (err, req, res, next) => {
-    if (err instanceof HttpError) {
-       res.status(err.status).json({
-        status:err.status,
-        message: err.name,
-        error: err,
-       });  
-        return;
-    }
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
 
-    res.stutus(err.status).json({
-        status: err.status,
-        message: err.message,
-        data: err.message
+  // Якщо невалідний ObjectId
+  if (err.name === 'CastError' && err.kind === 'ObjectId') {
+    return res.status(404).json({
+      status: 404,
+      message: 'Contact not found!',
     });
-   
-    res.status(500).json({
-        status: 500,
-        message: 'Something went wrong',
-        data: err.message,
-    });
+  }
+
+  // Всі інші помилки
+  res.status(500).json({
+    status: 500,
+    message: 'Something went wrong',
+  });
 };
--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -2,5 +2,4 @@ import createHttpError from 'http-errors';
 
 export const notFoundHandler = (req, res, next) => {
     next(createHttpError(404, 'Route not found!'));
-   
 };
