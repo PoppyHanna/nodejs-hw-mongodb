bba5503fad4f499c8ddbd94d6ff8b8cfb202b692
PoppyHanna <142433975+PoppyHanna@users.noreply.github.com>
2025-05-22 21:35:55 -0400

    create 1

---
+++ src/validation/contacts.js
@@ -0,0 +1,17 @@
+import Joi from "joi";
+
+export const createContactSchema = Joi.object({
+    name: Joi.string().min(3).max(30).required(),
+    phoneNumber: Joi.string().pattern(/^(\+?38)?[\s-]?\(?0\d{2}\)?[\s-]?\d{3}[\s-]?\d{2}[\s-]?\d{2}$/).required(),
+    email: Joi.string().email({ maxDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+    isFavourite: Joi.boolean(),
+    contactType: Joi.string().valid('work', 'home', 'personal').required(),
+});
+
+export const updateContactSchema = Joi.object({
+    name: Joi.string().min(3).max(20),
+    phoneNumber: Joi.string(),
+    email: Joi.string().email(),
+    isFavourite: Joi.boolean(),
+    contactType: Joi.string().valid('work', 'home', 'personal'),
+}).min(1);
